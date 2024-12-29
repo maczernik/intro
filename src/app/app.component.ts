796d23b59338835fc40a86da50d393989aa33e08
CZERNIK Maciej <maciej.czernik-ext@leica-geosystems.com>
2024-12-29 10:55:11 +0100

    INTRO: Change app prefix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'in-app-root',
+  selector: 'in-root',
   styleUrls: ['./app.component.scss'],
   templateUrl: './app.component.html',
 })
