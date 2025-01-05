777b9d6cdefe7f9ee3f267b29cc97dd73dedff56
CZERNIK Maciej <maciej.czernik-ext@leica-geosystems.com>
2025-01-05 19:42:21 +0100

    INTRO: Improve styling

--- src/app/intro-components/components/template/template.component.ts
+++ src/app/intro-components/components/template/template.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { map, interval, startWith } from 'rxjs';
 
 import { TemplateSnackComponent } from 'src/app/intro-components/components/template/components/template-snack.component';
 
@@ -15,4 +16,10 @@ import { TemplateSnackComponent } from 'src/app/intro-components/components/temp
 })
 export class TemplateComponent {
   protected componentVariable = 'Test text';
+
+  // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+  protected asyncVariable$ = interval(5000).pipe(
+    startWith(-1),
+    map((e) => `${e}`),
+  );
 }
